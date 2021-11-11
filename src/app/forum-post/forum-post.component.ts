6fa7509b8015706f8e012b46ce3fcfe0012243fb
Matthew Cumbo <mattcumbo99@gmail.com>
2021-11-11 13:38:50 -0600

    Styling

--- src/app/forum-post/forum-post.component.ts
+++ src/app/forum-post/forum-post.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalVariables } from '../common/global-variables';
+import { Title } from '@angular/platform-browser';
+import { UserService } from '../backend/services/user.service';
+import { ForumpostService } from '../backend/services/forumpost.service';
+import { Router } from '@angular/router';
+import { ForumPost } from '../backend/interfaces/forumpost.interface';
+import { User } from '../backend/interfaces/user.interface';
+import { ForumComment } from '../backend/interfaces/forumcomment.interface';
 
 @Component({
   selector: 'app-forum-post',
@@ -7,9 +15,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForumPostComponent implements OnInit {
 
-  constructor() { }
+  // Post data
+  postData:any;
+  postUser:User = this.globals.defaultUser;
+
+  constructor(public globals:GlobalVariables, private titleService:Title, private userService:UserService,
+    private router:Router, private forumPostService:ForumpostService) { }
 
   ngOnInit(): void {
+    // Get the thread ID from the url
+    const thNumber = this.router.url.split("/").pop();
+
+    if (thNumber != undefined) {
+      const threadId:number = +thNumber;
+
+      // Get the post data from the corresponding thread id
+      this.forumPostService.getPost(threadId).subscribe(data=> {
+        this.postData = data;
+        this.titleService.setTitle(data.title + " | " + this.globals.websiteTitle);
+
+        // Get the user from the post
+        this.userService.getUser(data.author).subscribe(userData=> {
+          this.postUser = userData;
+        });
+
+      });
+    }
   }
 
 }
